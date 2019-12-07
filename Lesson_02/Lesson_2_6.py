cac4a79527214818643f0d76faca7927ab63b451
ss2576 <ss2576@mail.ru>
2019-12-07 18:01:10 +0300

    изменения на 7 декабря 2019

--- Lesson_02/Lesson_2_6.py
+++ Lesson_02/Lesson_2_6.py
@@ -3,26 +3,25 @@
 # cообщаться больше или меньше введенное пользователем число, чем то, что загадано.
 # Если за 10 попыток число не отгадано, то вывести загаданное число.
 
-import  random
+import random
 
 number = random.randint(0, 100)
 # оставил подсказку для проверки
-print(number)
-
+print(f'Подсказка:  загаданное число -{number}-')
 
 user_number = None
 count = 0
 
 while number != user_number:
-	count += 1
-	if count > 10:
-		print(f'Вы проиграли!!!\nЗагаданное число -{user_number}-')
-		break
-	print(f'Попытка № {count}')
-	user_number = int(input('Введите число: \n'))
-	if number < user_number:
-		print('Ваше число больше загаданного!')
-	elif number > user_number:
-		print('Ваше число меньше загаданного!')
+    count += 1
+    if count > 10:
+        print(f'Вы проиграли!!!\nЗагаданное число -{user_number}-')
+        break
+    print(f'Попытка № {count}')
+    user_number = int(input('Введите число: \n'))
+    if number < user_number:
+        print('Ваше число больше загаданного!')
+    elif number > user_number:
+        print('Ваше число меньше загаданного!')
 else:
-	print('Победа!вы угадали!!!')
+    print('Победа!вы угадали!!!')
