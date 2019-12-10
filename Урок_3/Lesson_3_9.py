29a4fcc4358d587691c603bf4b9bd146df94a60c
ss2576 <ss2576@mail.ru>
2019-12-10 15:53:11 +0300

    добавлен Урок 3

---
+++ Урок_3/Lesson_3_9.py
@@ -0,0 +1,30 @@
+# Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+
+
+import random
+
+l = int(input('какой длины сделать матрицу ? :\n'))
+h = int(input('какой высоты сделать матрицу ? :\n'))
+max_number = int(input('какая максимальная цифра в  матрице ? :\n'))
+arr = []
+for i in range(h):
+	a = []
+	for j in range(l):
+		n = random.randint(1, max_number)
+		a.append(n)
+		print(n, ' ', end='')
+	arr.append(a)
+	print()
+
+max_ = -1
+for j in range(l):
+	min_ = max_number
+	for i in range(h):
+		if arr[i][j] < min_:
+			min_ = arr[i][j]
+
+	if min_ > max_:
+		max_ = min_
+
+print()
+print(f'Максимальный среди минимальных: {max_}')
