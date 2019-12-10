8c63921d4d27eb60c1caa43aeff020d46a86df2b
ss2576 <ss2576@mail.ru>
2019-12-10 21:49:16 +0300

    10.12.2019

--- Урок_3/Lesson_3_3.py
+++ Урок_3/Lesson_3_3.py
@@ -6,12 +6,17 @@ L = (int(input('сколько сгенерировать чисел в стро
 max_number = int(input('какая максимальная цифра в  матрице ? :\n'))
 arr = []
 for i in range(L):
+<<<<<<< HEAD
 	arr.append(random.randint(1, max_number))
+=======
+    arr.append(random.randint(1, max_number))
+>>>>>>> изм на 10.12.2019
 print(arr)
 
 min_ = 0
 max_ = 0
 for i in range(L):
+<<<<<<< HEAD
 	if arr[i] < arr[min_]:
 		min_ = i
 	elif arr[i] > arr[max_]:
@@ -25,3 +30,16 @@ arr[min_] = arr[max_]
 arr[max_] = temp
 
 print('массив после замены: \n', arr)
+=======
+    if arr[i] < arr[min_]:
+        min_ = i
+    elif arr[i] > arr[max_]:
+        max_ = i
+
+print(f'менятся будут местами: \nмаксимальное число {arr[max_]} (порядковый № {max_ + 1}) и \n'
+      f'минимальное число {arr[min_]} (порядковый № {min_ + 1})')
+
+arr[min_], arr[max_] = arr[max_], arr[min_]
+
+print('массив после замены: \n', arr)
+>>>>>>> изм на 10.12.2019
